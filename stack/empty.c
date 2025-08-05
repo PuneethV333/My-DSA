f24b62a34b15df578a5af2e1a384cef24c4c756c
PuneethV333 <puneethvijayakumar333@gmail.com>
2025-08-05 21:16:00 +0530

    empty.c

---
+++ stack/empty.c
@@ -0,0 +1,32 @@
+#define stack_size 100
+#include <stdlib.h>
+#include <stdio.h>
+
+typedef struct stack{
+    int top;
+    int item[stack_size];
+}stack_t;
+
+int empty(stack_t *ps){
+    if(ps->top == -1){
+        return 1;
+    }else{
+        return 0;
+    }
+}
+
+
+int main() {
+    stack_t s;
+    s.top = 12;
+    for (int i = 0; i < s.top; i++){
+        s.item[i] = i;
+    }
+    int x = empty(&s);
+    if(x == 1){
+        printf("stack is empty");
+    }else{
+        printf("stack is not empty");
+    }
+    return 0;
+}
